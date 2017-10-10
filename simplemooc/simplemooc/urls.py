69ce6bf664eaf44a0a707da231c14d7764e3d85a
Ricardo Teixeira <ricardocdzg@hotmail.com>
2017-10-09 22:27:44 -0300

    Including models

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from simplemooc.core import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^contato$', views.contact, name='contact'),
+    url(r'^', include('simplemooc.core.urls', namespace='core')),
     url(r'^admin/', admin.site.urls),
 ]
