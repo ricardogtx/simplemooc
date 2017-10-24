6ed672ee91d3979dbad7b326b3e033d1dbb8529a
Ricardo Teixeira <ricardocdzg@hotmail.com>
2017-10-23 22:34:42 -0200

    Adding forms

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourse
 
 
 def index(request):
@@ -11,10 +12,27 @@ def index(request):
     }
     return render(request, template_name, context)
 
-def details(request, pk):
-    course = Course.objects.get(pk=pk)
-    context = {
-        'course': course
-    }
+# def details(request, pk):
+#     course = get_object_or_404(Course, pk=pk)
+#     context = {
+#         'course': course
+#     }
+#     template_name = 'courses/details.html'
+#     return render(request, template_name, context)
+
+
+def details(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    context = {}
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+
+    context['form'] = form
+    context['course'] = course
     template_name = 'courses/details.html'
     return render(request, template_name, context)
--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -124,3 +124,13 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
+
+#E-mails
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL ='Nome <email@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'user@gmail.com'
+EMAIL_HOST_PASSWORD = 'senha'
+EMAIL_PORT = 587
