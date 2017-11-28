f679e843e62e1573c1087a93fefee59fbee3458b
Ricardo Teixeira <ricardocdzg@hotmail.com>
2017-11-27 23:18:55 -0200

    Authorization mechanism

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
-from .views import register
+from django.contrib.auth.views import logout
+from .views import register,dashboard, edit
 
 urlpatterns = [
+    url(r'^/$', dashboard, name='dashboard'),
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^sair/$', logout, {'next_page': 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', register, name='register'),
+    url(r'^editar/$', edit, name='edit')
 ]
--- simplemooc/simplemooc/accounts/views.py
+++ simplemooc/simplemooc/accounts/views.py
@@ -1,21 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
+
+@login_required
+def dashboard(request):
+    template_name='accounts/dashboard.html'
+    context = {}
+    return render(request, template_name, context)
 
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
-        print("Post")
         form = RegisterForm(request.POST)
         if form.is_valid():
-            print("Válido")
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(
+                username = user.username,
+                password = form.cleaned_data['password1']
+            )
+            login(request, user)
+
+            return redirect('core:home')
     else:
         form = RegisterForm()
     context = {
         'form': form
     }
     return render(request, template_name, context)
+
+@login_required
+def edit(request):
+    template_name='accounts/edit.html'
+    form = EditAccountForm
+    context = {}
+    context['form'] = form
+    return render(request, template_name)
