b54711e0f85e3a42ec293eb86795e71e6b087009
Ricardo Teixeira <ricardocdzg@hotmail.com>
2018-02-19 23:39:32 -0300

    Adding user mechanism

--- simplemooc/simplemooc/accounts/urls.py
+++ simplemooc/simplemooc/accounts/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login
-from django.contrib.auth.views import logout
-from .views import register,dashboard, edit
+from django.contrib.auth.views import login as auth_login
+from django.contrib.auth.views import logout as auth_logout
+
+from .views import *
 
 urlpatterns = [
-    url(r'^/$', dashboard, name='dashboard'),
-    url(r'^entrar/$', login, {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^sair/$', logout, {'next_page': 'core:home'}, name='logout'),
+    url(r'^$', dashboard, name='dashboard'),
+    url(r'^entrar/$', auth_login,
+     {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^sair/$', auth_logout,
+     {'next_page': 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', register, name='register'),
-    url(r'^editar/$', edit, name='edit')
+    url(r'^editar/$', edit, name='edit'),
+    url(r'^editar-senha/$', edit_password, name='edit_password')
 ]
