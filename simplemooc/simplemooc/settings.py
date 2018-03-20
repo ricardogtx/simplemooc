ae833fd85bf30e28b4cdb2e9d8dbd1d8a6609cd0
Ricardo Teixeira <ricardocdzg@hotmail.com>
2018-03-19 23:10:03 -0300

    Adding reset password sending

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -127,14 +127,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 MEDIA_URL = '/media/'
 
 #E-mails
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL ='Nome <email@gmail.com>'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'user@gmail.com'
-EMAIL_HOST_PASSWORD = 'senha'
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'user@gmail.com'
+# EMAIL_HOST_PASSWORD = 'senha'
+# EMAIL_PORT = 587
 
 CONTACT_EMAIL = 'contato@simplemooc.com'
 
