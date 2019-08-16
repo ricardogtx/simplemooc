dbf37bdf4cac70f5aaeee2bcb251360fe5ecc6d2
Ricardo <ricardocdzg@gmail.com>
2019-08-15 23:16:52 -0300

    Ad listing

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -70,7 +70,9 @@ class Enrollment(models.Model):
 
 class Annoucement(models.Model):
 
-    course = models.ForeignKey(Course, verbose_name='Curso')
+    course = models.ForeignKey(
+        Course, verbose_name='Curso', related_name='announcements'
+    )
     title = models.CharField('Título', max_length=100)
     content = models.TextField('Conteúdo')
 
