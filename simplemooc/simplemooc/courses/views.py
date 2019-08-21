8cda8c7623e29c60a3cf511c0f4327508c5cad51
Ricardo <ricardocdzg@gmail.com>
2019-08-20 22:08:43 -0300

    Ad page and comments

--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Annoucement
 from .forms import ContactCourse
 
 
@@ -85,5 +85,24 @@ def announcements(request, slug):
     context = {
         'course': course,
         'announcements': course.announcements.all()
+    }
+    return render(request, template, context)
+
+@login_required
+def show_announcement(request, slug, pk):
+    course = get_object_or_404(Course, slug=slug)
+    if not  request.user.is_staff:
+        enrollment = get_object_or_404(
+            Enrollment, user=request.user, course=course
+        )
+        if not enrollment.is_approved():
+            messages.error(request, 'A sua incrição está pendente')
+            return redirect('accounts:dashboard')
+    
+    template = 'courses/show_acnnoucement.html'
+    announcement = get_object_or_404(course.announcements.all(), pk=pk)
+    context = {
+        'course' : course,
+        'annoucement' : announcement,
     }
     return render(request, template, context)
