e27f92ce9336119403194bfdbd129cc03229d0e3
Ricardo <ricardocdzg@gmail.com>
2019-09-11 22:45:19 -0300

    Setting up heroku server

--- simplemooc/simplemooc/wsgi.py
+++ simplemooc/simplemooc/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
 application = get_wsgi_application()
