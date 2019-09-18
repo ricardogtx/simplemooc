4ea06121380ce3f4fac48a1534e6fd31675d5681
Ricardo <ricardocdzg@gmail.com>
2019-09-17 23:29:37 -0300

    Fix static files

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -148,10 +148,6 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 try:
     from simplemooc.local_settings import *
 except ImportError:
