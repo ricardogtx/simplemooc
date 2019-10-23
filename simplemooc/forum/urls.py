dd7157c0683185a47990b233483c899e87dc58c8
Ricardo <ricardocdzg@gmail.com>
2019-10-22 23:56:09 -0300

    Improve forum

--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    #url(r'^(?P<pk>\d+)/$', views.details, name='details'),
+    url(r'^tag(?P<tag>[\w_-]+)/$', views.index, name='index_tagged'),
 ]
--- simplemooc/forum/views.py
+++ simplemooc/forum/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View, ListView
 
-class ForumView(TemplateView):
+from .models import Thread
 
+class ForumView(ListView):
+
+    paginate_by = 2
     template_name = 'forum/index.html'
+
+    def get_queryset(self):
+        queryset = Thread.objects.all()
+        order = self.request.GET.get('order','')
+        if order == 'views':
+            queryset = queryset.order_by('-views')
+        elif order == 'answers':
+            queryset = queryset.order_by('-answers')
+
+        tag = self.kwargs.get('tag','')
+        if tag:
+            queryset = queryset.filter(tags__slug__icontains=tag)
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super(ForumView,self).get_context_data(**kwargs)
+        context['tags'] = Thread.tags.all()
+        return context
 
 index = ForumView.as_view()
